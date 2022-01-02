3d4617fd7522d798996c7e4c596b9d514ceabea4
dpbam <derekphtpt@gmail.com>
2022-01-02 15:57:56 -0700

    styled At Scope Now button and made hyperlink to correct page

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function openSlideMenu() {
+  document.getElementById('side-menu').style.width = '250px';
+  document.getElementById('main').style.marginLeft = '250px';
+}
+function closeSlideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
+}
