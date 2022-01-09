1515af588184730339f0f4d87eed7a5892a5ffc7
dpbam <derekphtpt@gmail.com>
2022-01-09 15:21:33 -0700

    twitter feed and news feed on main page working, but need to automate the news feed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,26 @@ function closeSlideMenu() {
   document.getElementById('side-menu').style.width = '0';
   document.getElementById('main').style.marginLeft = '0';
 }
+
+// monitoring Twitter using API (also live streaming)
+
+const Twit = require('twit')
+const nofifier = require('node-notifier');
+const open = require('open');
+const franc = require('franc')
+
+const apiKey = ''
+const apiSecretKey = ''
+const accessToken = ''
+const accessTokenSecret = ''
+
+var T = new Twit({
+  consumer_key: apiKey,
+  consumer_secret: apiSecretKey,
+  access_token: accessToken,
+  access_token_secret: accessTokenSecret,
+});
+
+(async () => {
+  
+})
