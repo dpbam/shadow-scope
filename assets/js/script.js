d53390ff19fbe3355a0d6379cca101ee6add5f16
dpbam <derekphtpt@gmail.com>
2022-02-08 16:14:00 -0700

    feature/videos-dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,20 +63,81 @@ function showSlides() {
 // End user controls slideshow
 
 // videos search bar
-var videos = [10, 20, 30, 40, 50];
-var myInputs = document.getElementById('myInput');
+var videos = [
+  { name: 'Video001' },
+  { name: 'Video002' },
+  { name: 'Video003' },
+  { name: 'Video004' },
+  { name: 'Video005' },
+  { name: 'Video006' },
+  { name: 'Video007' },
+  { name: 'Video008' },
+];
+var searchInput = document.querySelector('.input');
 var searchButton = document.getElementById('search-button');
 
-function searchFunction() {
-  if (videos.indexOf(Number(myInputs.value)) == -1) {
-    document.write(myInputs.value + '<b>Not Found</b>');
+searchButton.addEventListener('input', (e) => {
+  // inside, we will need to achieve a few things:
+  // 1. declare and assign the value of the event's target to a variable AKA whatever is typed into the search bar
+  let value = e.target.value;
+
+  // 2. check: if input exists and if input is larger than 0
+  if (value && value.trim().length > 0) {
+    // 3. redefine 'value' to exclude white space and change input to all lowercase
+    value = value.trim().toLowerCase();
+    // 4. return the results only if the value of the search is included in the video's name
+    // we need to write code (a function for filtering through our data to include the search input value)
+    searchButton.addEventListener('input', (e) => {
+      let value = e.target.value;
+
+      if (value && value.trim().length > 0) {
+        value = value.trim().toLowerCase();
+
+        // returning only the results of setList if the value of the search is included in the video's name
+        setList(
+          videos.filter((video) => {
+            return video.name.includes(value);
+          })
+        );
+      }
+    });
   } else {
-    document.write(
-      '<b>Element found at ' +
-        videos.indexOf(Number(myInputs.value)) +
-        ' Position</b>'
-    );
+    // 5. return nothing
+    // input is invalid -- show an error message or show no results
+  }
+});
+
+// creating and declaring a function called "setList"
+// setList takes in a param of "results"
+function setList(results) {
+  for (const video of results) {
+    // creating a li element for each result item
+    const resultItem = document.createElement('li');
+
+    // adding a class to each item of the results
+    resultItem.classList.add('result-item');
+
+    // grabbing the name of the current point of the loop and adding the name as the list item's text
+    const text = document.createTextNode(video.name);
+
+    // appending the text to the result item
+    resultItem.appendChild(text);
+
+    // appending the result item to the list
+    list.appendChild(resultItem);
   }
 }
 
-searchButton.addEventListener('click', searchFunction);
+// function videosSearchFunction() {
+//   if (videos.indexOf(Number(myInputs.value)) == -1) {
+//     document.write(myInputs.value + '<b>Not Found</b>');
+//   } else {
+//     document.write(
+//       '<b>Element found at ' +
+//         videos.indexOf(Number(myInputs.value)) +
+//         ' Position</b>'
+//     );
+//   }
+// }
+
+// searchButton.addEventListener('click', searchFunction);
