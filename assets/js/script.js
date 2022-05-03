8250dc57f50d4b0304f73dce43569d6ffdd3158f
dpbam <derekphtpt@gmail.com>
2022-05-03 15:09:43 -0600

    search function trouble shooting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1264,60 +1264,60 @@ function SubForm(video) {
 // pagination
 
 // const list_element = document.getElementById('list');
-const list_element = document.getElementById('videos-results');
-const pagination_element = document.getElementById('pagination');
+// const list_element = document.getElementById('videos-results');
+// const pagination_element = document.getElementById('pagination');
 
-let current_page = 1;
-let rows = 5;
+// let current_page = 1;
+// let rows = 5;
 
-// displaylist function
-function DisplayList(items, wrapper, rows_per_page, page) {
-  wrapper.innerHTML = '';
-  page--;
+// // displaylist function
+// function DisplayList(items, wrapper, rows_per_page, page) {
+//   wrapper.innerHTML = '';
+//   page--;
 
-  let start = rows_per_page * page;
-  let end = start + rows_per_page;
-  let paginatedItems = items.slice(start, end);
+//   let start = rows_per_page * page;
+//   let end = start + rows_per_page;
+//   let paginatedItems = items.slice(start, end);
 
-  for (let i = 0; i < paginatedItems.length + rows_per_page; i++) {
-    let item = paginatedItems[i];
+//   for (let i = 0; i < paginatedItems.length + rows_per_page; i++) {
+//     let item = paginatedItems[i];
 
-    let item_element = document.createElement('div');
-    item_element.classList.add('item');
-    item_element.innerText = item;
+//     let item_element = document.createElement('div');
+//     item_element.classList.add('item');
+//     item_element.innerText = item;
 
-    wrapper.appendChild(item_element);
-  }
-}
+//     wrapper.appendChild(item_element);
+//   }
+// }
 
-function SetupPagination(items, wrapper, rows_per_page) {
-  wrapper.innerHTML = '';
+// function SetupPagination(items, wrapper, rows_per_page) {
+//   wrapper.innerHTML = '';
 
-  let page_count = Math.ceil(items.length / rows_per_page);
-  for (let i = 1; i < page_count + 1; i++) {
-    let btn = PaginationButton(i, items);
-    wrapper.appendChild(btn);
-  }
-}
+//   let page_count = Math.ceil(items.length / rows_per_page);
+//   for (let i = 1; i < page_count + 1; i++) {
+//     let btn = PaginationButton(i, items);
+//     wrapper.appendChild(btn);
+//   }
+// }
 
-function PaginationButton(page, items) {
-  let button = document.createElement('button');
-  button.innerText = page;
+// function PaginationButton(page, items) {
+//   let button = document.createElement('button');
+//   button.innerText = page;
 
-  if (current_page == page) button.classList.add('active');
+//   if (current_page == page) button.classList.add('active');
 
-  button.addEventListener('click', function () {
-    current_page = page;
-    DisplayList(items, list_element, rows, current_page);
+//   button.addEventListener('click', function () {
+//     current_page = page;
+//     DisplayList(items, list_element, rows, current_page);
 
-    let current_btn = document.querySelector('.pagenumbers button.active');
-    current_btn.classList.remove('active');
+//     let current_btn = document.querySelector('.pagenumbers button.active');
+//     current_btn.classList.remove('active');
 
-    button.classList.add('active');
-  });
+//     button.classList.add('active');
+//   });
 
-  return button;
-}
+//   return button;
+// }
 
-DisplayList(videos, list_element, rows, current_page);
-SetupPagination(videos, pagination_element, rows);
+// DisplayList(videos, list_element, rows, current_page);
+// SetupPagination(videos, pagination_element, rows);
